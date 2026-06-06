97c094f824cc14f1aae1e3cf807ff541c3a3b58e
SCBProd <scbproduktion@gmail.com>
2026-06-06 13:42:32 +0200

    1

--- src/server.js
+++ src/server.js
@@ -1,77 +1,35 @@
 // src/server.js
-import express from 'express';
-import cors from 'cors';
-import pino from 'pino-http';
-import 'dotenv/config';
 
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+
+import studentsRoutes from './routes/studentsRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-// Middleware
-app.use(express.json());
+// глобальні middleware
+app.use(logger);
+app.use(express.json({
+  limit: '100kb', // максимум 100 кілобайт
+}));
 app.use(cors());
-app.use(
-  pino({
-    level: 'info',
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'HH:MM:ss',
-        ignore: 'pid,hostname',
-        messageFormat: '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
-        hideObject: true,
-      },
-    },
-  }),
-);
-
-// Кореневий маршрут
-app.get('/', (req, res) => {
- res.status(200).json({ message: 'Hello, World!' });
-});
 
 
-// Кореневий маршрут
-app.get('/notes', (req, res) => {
- res.status(200).json({ message: "Retrieved all notes" });
-});
+// підключаємо групу маршрутів студента
+app.use(studentsRoutes);
 
-app.get('/notes/:noteId', (req, res) => {
-  const { noteId } = req.params;
-  res.status(200).json({ message: `Retrieved note with ID: ${noteId}` });
-});
+// 404 і обробник помилок — наприкінці ланцюжка
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-
-// Маршрут для тестування middleware помилки
-app.get('/test-error', (req, res) => {
- // Штучна помилка для прикладу
- throw new Error('Something went wrong');
-});
-
-// Middleware 404 (після всіх маршрутів)
-app.use((req, res) => {
- res.status(404).json({ message: 'Route not found' });
-});
-
-
-// Middleware для обробки помилок
-app.use((err, req, res, next) => {
-  console.error(err);
-
-  const isProd = process.env.NODE_ENV === "production";
-
-  res.status(500).json({
-    message: isProd
-      ? "Something went wrong. Please try again later."
-      : err.message,
-  });
-});
-
-app.get('/test-error', (req, res) => {
-  throw new Error('Simulated server error');
-});
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
