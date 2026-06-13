6b22fb94019a4161cf3d89cffd83ab984699e460
SCBProd <scbproduktion@gmail.com>
2026-06-13 16:40:05 +0200

    3

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -9,6 +9,33 @@ const isValidObjectId = (value, helpers) => {
   return value;
 };
 
+// GET /notes
+export const getAllNotesSchema = celebrate({
+  [Segments.QUERY]: Joi.object({
+    page: Joi.number().integer().min(1).default(1),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
+    tag: Joi.string().valid(...TAGS),
+    search: Joi.string().allow(''),
+  }),
+});
+
+// GET /notes/:noteId (і DELETE теж)
+export const noteIdSchema = celebrate({
+  [Segments.PARAMS]: Joi.object({
+    noteId: Joi.string().custom(isValidObjectId).required(),
+  }),
+});
+
+// POST /notes
+export const createNoteSchema = celebrate({
+  [Segments.BODY]: Joi.object({
+    title: Joi.string().min(1).required(),
+    content: Joi.string().allow('').optional(),
+    tag: Joi.string().valid(...TAGS),
+  }),
+});
+
+// PATCH /notes/:noteId
 export const updateNoteSchema = celebrate({
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom(isValidObjectId).required(),
@@ -19,9 +46,6 @@ export const updateNoteSchema = celebrate({
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   })
-    .or('title', 'content', 'tag') 
-    .required()
-    .messages({
-      'object.missing': 'At least one field (title, content, tag) is required',
-    }),
+    .or('title', 'content', 'tag')
+    .required(),
 });
