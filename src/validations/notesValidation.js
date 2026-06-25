8e1c9e7938a5e123f1b85e09894dd97ca4881fb7
SCBProd <scbproduktion@gmail.com>
2026-06-26 00:40:22 +0200

    3.2

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,4 +1,4 @@
-import { celebrate, Joi, Segments } from 'celebrate';
+import { Joi, Segments } from 'celebrate';
 import mongoose from 'mongoose';
 import { TAGS } from '../constants/tags.js';
 
@@ -10,33 +10,33 @@ const isValidObjectId = (value, helpers) => {
 };
 
 // GET /notes
-export const getAllNotesSchema = celebrate({
+export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string().allow(''),
   }),
-});
+};
 
-// GET /notes/:noteId (і DELETE теж)
-export const noteIdSchema = celebrate({
+// GET /notes/:noteId
+export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom(isValidObjectId).required(),
   }),
-});
+};
 
 // POST /notes
-export const createNoteSchema = celebrate({
+export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow('').optional(),
     tag: Joi.string().valid(...TAGS),
   }),
-});
+};
 
 // PATCH /notes/:noteId
-export const updateNoteSchema = celebrate({
+export const updateNoteSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom(isValidObjectId).required(),
   }),
@@ -45,12 +45,10 @@ export const updateNoteSchema = celebrate({
     title: Joi.string().min(1),
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
-  })
-    .or('title', 'content', 'tag')
-    .required(),
-});
+  }).or('title', 'content', 'tag'),
+};
 
-/// register
+// REGISTER USER
 export const registerUserSchema = {
   [Segments.BODY]: Joi.object({
     email: Joi.string().email().required(),
