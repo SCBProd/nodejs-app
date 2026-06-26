4b3076991879f918ecdac635a1b765916045eeb9
SCBProd <scbproduktion@gmail.com>
2026-06-26 21:32:25 +0200

    2

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,16 +1,20 @@
-// src/utils/sendEmail.js
-
 import nodemailer from 'nodemailer';
 
-const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: process.env.SMTP_PORT,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
-  },
-});
+export const sendEmail = async ({ from, to, subject, html }) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
 
-export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+  return transporter.sendMail({
+    from,
+    to,
+    subject,
+    html,
+  });
 };
